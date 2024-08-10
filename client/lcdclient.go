dbbc5b46c5de1520678695a876a602ea5d5982d5
liuhaoyangz <liuhaoyangz@didiglobal.com>
2024-08-10 09:22:12 +0800

    add code

--- client/lcdclient.go
+++ client/lcdclient.go
@@ -142,32 +142,37 @@ func (lcd *LCDClient) GetAddress() msg.AccAddress {
 	return msg.AccAddress(lcd.PrivKey.PubKey().Address())
 }
 
-func (lcd *LCDClient) CreateDataset(ctx context.Context, options CreateTxOptions, datasetName string, workStatus chaindepindextype.ServiceStatus, hosts string, manageAddresses string, meta string, description string, duration int64) (*sdk.TxResponse, error) {
+func (lcd *LCDClient) CreateDataset(ctx context.Context, datasetName string, workStatus chaindepindextype.ServiceStatus, hosts string, manageAddresses string, meta string, description string, duration int64) (*sdk.TxResponse, error) {
 	_msg := chaindepindextype.NewCreateDatasetRequest(lcd.GetAddress(), datasetName, workStatus, hosts, manageAddresses, meta, description, duration)
+	options := CreateTxOptions{SignMode: tx.SignModeDirect}
 	options.Msgs = []msg.Msg{_msg}
 	return lcd.SignAndBroadcastTX(ctx, options)
 }
 
-func (lcd *LCDClient) EditDatasetRequest(ctx context.Context, options CreateTxOptions, datasetName string, workStatus chaindepindextype.ServiceStatus, hosts string, manageAddresses string, meta string, description string) (*sdk.TxResponse, error) {
+func (lcd *LCDClient) EditDatasetRequest(ctx context.Context, datasetName string, workStatus chaindepindextype.ServiceStatus, hosts string, manageAddresses string, meta string, description string) (*sdk.TxResponse, error) {
 	_msg := chaindepindextype.NewEditDatasetRequest(lcd.GetAddress(), datasetName, workStatus, hosts, manageAddresses, meta, description)
+	options := CreateTxOptions{SignMode: tx.SignModeDirect}
 	options.Msgs = []msg.Msg{_msg}
 	return lcd.SignAndBroadcastTX(ctx, options)
 }
 
-func (lcd *LCDClient) RenewalDataset(ctx context.Context, options CreateTxOptions, datasetName string, duration int64) (*sdk.TxResponse, error) {
+func (lcd *LCDClient) RenewalDataset(ctx context.Context, datasetName string, duration int64) (*sdk.TxResponse, error) {
 	_msg := chaindepindextype.NewRenewalDatasetRequest(lcd.GetAddress(), datasetName, duration)
+	options := CreateTxOptions{SignMode: tx.SignModeDirect}
 	options.Msgs = []msg.Msg{_msg}
 	return lcd.SignAndBroadcastTX(ctx, options)
 }
 
-func (lcd *LCDClient) Pledge(ctx context.Context, options CreateTxOptions, datasetName string, amount sdk.Int) (*sdk.TxResponse, error) {
+func (lcd *LCDClient) Pledge(ctx context.Context, datasetName string, amount sdk.Int) (*sdk.TxResponse, error) {
 	_msg := chaindepconsumertype.NewPledgeRequest(lcd.GetAddress(), datasetName, amount)
+	options := CreateTxOptions{SignMode: tx.SignModeDirect}
 	options.Msgs = []msg.Msg{_msg}
 	return lcd.SignAndBroadcastTX(ctx, options)
 }
 
-func (lcd *LCDClient) ReleasePledge(ctx context.Context, options CreateTxOptions, datasetName string, amount sdk.Int) (*sdk.TxResponse, error) {
+func (lcd *LCDClient) ReleasePledge(ctx context.Context, datasetName string, amount sdk.Int) (*sdk.TxResponse, error) {
 	_msg := chaindepconsumertype.NewReleasePledgeRequest(lcd.GetAddress(), datasetName, amount)
+	options := CreateTxOptions{SignMode: tx.SignModeDirect}
 	options.Msgs = []msg.Msg{_msg}
 	return lcd.SignAndBroadcastTX(ctx, options)
 }
