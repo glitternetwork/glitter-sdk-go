0a14694ff11ebdc7099aa884e32c4839e2b321c0
liuhaoyangz <liuhaoyangz@didiglobal.com>
2024-09-27 17:55:32 +0800

    add code

--- client/write.go
+++ client/write.go
@@ -9,14 +9,14 @@ import (
 	"github.com/glitternetwork/glitter-sdk-go/tx"
 )
 
-func (lcd *LCDClient) CreateDataset(ctx context.Context, datasetName string, workStatus chaindepindextype.ServiceStatus, hosts string, manageAddresses string, meta string, description string, duration int64) (*sdk.TxResponse, error) {
+func (lcd *LCDClient) CreateDataset(ctx context.Context, datasetName string, workStatus chaindepindextype.ServiceStatus, hosts string, manageAddresses string, description string, duration int64) (*sdk.TxResponse, error) {
 	_msg := chaindepindextype.NewCreateDatasetRequest(lcd.GetAddress(), datasetName, workStatus, hosts, manageAddresses, description, duration)
 	options := CreateTxOptions{SignMode: tx.SignModeDirect}
 	options.Msgs = []msg.Msg{_msg}
 	return lcd.SignAndBroadcastTX(ctx, options)
 }
 
-func (lcd *LCDClient) EditDataset(ctx context.Context, datasetName string, workStatus chaindepindextype.ServiceStatus, hosts string, manageAddresses string, meta string, description string) (*sdk.TxResponse, error) {
+func (lcd *LCDClient) EditDataset(ctx context.Context, datasetName string, workStatus chaindepindextype.ServiceStatus, hosts string, manageAddresses string, description string) (*sdk.TxResponse, error) {
 	_msg := chaindepindextype.NewEditDatasetRequest(lcd.GetAddress(), datasetName, workStatus, hosts, manageAddresses, description)
 	options := CreateTxOptions{SignMode: tx.SignModeDirect}
 	options.Msgs = []msg.Msg{_msg}
